1ec88e40335275a18e63a0912e24859cf52f1f94
totoogg <goncharov.vova2014@gmail.com>
2025-07-26 17:32:20 +0300

    feat: add tests for about, error and cat pages

---
+++ tests/pages/ErrorPage.spec.tsx
@@ -0,0 +1,53 @@
+import { ErrorPage } from '../../src/pages/ErrorPage/ui/ErrorPage';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { vi, describe, it, expect, beforeEach, afterEach } from 'vitest';
+import { MemoryRouter } from 'react-router';
+
+const mockNavigate = vi.fn();
+
+beforeEach(() => {
+  vi.mock('react-router', async () => {
+    const mod =
+      await vi.importActual<typeof import('react-router')>('react-router');
+    return {
+      ...mod,
+      useNavigate: () => mockNavigate,
+    };
+  });
+});
+
+afterEach(() => {
+  mockNavigate.mockReset();
+});
+
+describe('ErrorPage', () => {
+  it('renders ErrorPage', () => {
+    render(
+      <MemoryRouter>
+        <ErrorPage />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText('404 NotFound')).toBeInTheDocument();
+    expect(
+      screen.getByText('Sorry, an unexpected error has occurred.')
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: 'Home page' })
+    ).toBeInTheDocument();
+  });
+
+  it('navigates to home page', () => {
+    render(
+      <MemoryRouter>
+        <ErrorPage />
+      </MemoryRouter>
+    );
+
+    const homeButton = screen.getByRole('button', { name: 'Home page' });
+    fireEvent.click(homeButton);
+
+    expect(mockNavigate).toHaveBeenCalledTimes(1);
+    expect(mockNavigate).toHaveBeenCalledWith('/?page=1');
+  });
+});
