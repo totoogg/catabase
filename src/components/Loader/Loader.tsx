3b1ee68d2366be18f95ac74f224bd6b5ae96f6d4
totoogg <goncharov.vova2014@gmail.com>
2025-08-30 11:40:48 +0300

    feat: add loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,9 @@
+import cls from './Loader.module.css';
+
+export const Loader = () => {
+  return (
+    <div className={cls.wrapper}>
+      <span className={cls.loader}></span>
+    </div>
+  );
+};
